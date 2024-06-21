b46c10abedcdcc245f2efd906d534968a7214499
Oliver Gallanti <63370357+OliverGallanti@users.noreply.github.com>
2024-06-22 01:14:32 +0200

    Update firebase.js

--- prueba-syntonize/src/firebase.js
+++ prueba-syntonize/src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // El deber ser es utilizar .env.local pero como debe correr en otro ordenador pongo la autenticación por aquí
+  // El deber ser es utilizar .env.local pero como debe correr en otro ordenador pongo la autenticación por aquí.
 
   // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   // authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -12,6 +12,7 @@ const firebaseConfig = {
   // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   // appId: import.meta.env.VITE_FIREBASE_APP_ID,
   
+  //Por cuestiones de seguridad el apiKey no debería estar en el repo. Pero por cuestiones de prueba, lo he puesto aquí para facilitar la revisión de la prueba
   apiKey: "AIzaSyABqvSHJiOQL3URZa2hOUqhXS5fyepmGuo",
   authDomain: "loginolivergallanti.firebaseapp.com",
   databaseURL:
